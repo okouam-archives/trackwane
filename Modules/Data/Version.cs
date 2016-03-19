7e1478957114db87b9916518bde9531b79218cbd
Olivier Kouame <olivier.kouame@salesforce.com>
2016-03-19 19:26:58 +0000

    More changes

--- Modules/Data/Version.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
-[assembly: AssemblyCompany("Wylesight")]
-[assembly: AssemblyProduct("Trackwane.Data")]
-[assembly: AssemblyCopyright("Copyright Wylesight 2016")]

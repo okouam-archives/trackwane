ef73211d804a8db4f71095e674d7a79aff8a0c46
Olivier Kouame <olivier.kouame@gmail.com>
2016-05-09 16:18:22 +0100

    Latest changes

--- Modules/AccessControl/AccessControl.E2E/test/organizations/commands/archive.organization.js
+++ Modules/AccessControl/AccessControl.E2E/test/organizations/commands/archive.organization.js
@@ -9,7 +9,39 @@ describe("Organizations :: Commands :: Archive Organization", function() {
 		ctx = {};
 	});
 
-	xit("returns a 204 when successful", function() {
+
+	it("returns a 400 when the organization does not exist", function() {
+		return fixtures
+			.makeSandboxApplication(ctx, api, defaults.APPLICATION_OWNER)
+			.then(function(result) {
+				return api.organizations.archive("RANDOM-KEY", true);
+			}).then(function(result) {
+				expect(result).to.have.status(400);
+			});
 	});
 
+	it("returns a 200 when successful", function() {
+		return fixtures
+			.makeSandboxApplication(ctx, api, defaults.APPLICATION_OWNER)
+			.then(function() {
+				return api.organizations.register("Nike", true);
+			}).then(function(result) {
+				return api.organizations.archive(result.body, true);
+			}).then(function(result) {
+				expect(result).to.have.status(200);
+			});
+	});
+
+	it("returns a 400 when the user is not a system manager", function() {
+		return fixtures.makeSandboxApplication(ctx, api, defaults.APPLICATION_OWNER,
+			[{ref: "Nike"}],
+			[{organization: "Nike", ref: "John"}])
+			.then(function(result) {
+				return api.login(ctx["Nike"]["John"].email, ctx["Nike"]["John"].password)
+			}).then(function(result) {
+				return api.organizations.archive(ctx["Nike"].key, true);
+			}).then(function(result) {
+				expect(result).to.have.status(400);
+			});
+	});
 });
